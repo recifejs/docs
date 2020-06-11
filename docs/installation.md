2843955bdefda66fc95c859c68503e3fed6f8bf7
André Lins <andrelucas01@hotmail.com>
2020-06-11 11:46:38 -0300

    Creating installation documentation

--- docs/installation.md
+++ docs/installation.md
@@ -3,4 +3,53 @@ id: installation
 title: Installation
 ---
 
-To Do
+The installation of RecifeJs is very simple and can be done in minutes.
+
+## Server requirements
+
+The structure of Recifejs needs very few requirements. See requirements below.
+
+- Git
+- Node >= 8.0
+- NPM >= 3.0
+
+## Installation RecifeJs CLI
+
+The RecifeJs CLI is the command-line interface for using all the powerful features of RecifeJs.
+
+<br />
+Install RecifeJs CLI:
+
+```bash
+npm install -g recife-cli
+```
+
+<br />
+To create a new project and see all the benefits of RecifeJs, run the following command:
+
+```bash
+recife project my-project-name
+cd my-project-name
+```
+
+<br />
+Start the new project with this command.
+
+```bash
+npm run server
+```
+
+<br />
+For more details visit the [CLI](introduction-cli.md) topic.
+
+## Using via npx
+
+If you do not want to install the CLI it is also possible to use it via npx. Below is an example of its use to create a new project.
+
+```bash
+npx recife-cli project my-project-name
+cd my-project-name
+npm run server
+```
+
+For more details visit the [CLI](introduction-cli.md) topic.
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,8 +12,6 @@
 const users = [
   {
     caption: "User1",
-    // You will need to prepend the image path with your baseUrl
-    // if it is not '/', like: '/test-site/img/image.jpg'.
     image: "/img/undraw_open_source.svg",
     infoLink: "https://www.facebook.com",
     pinned: true
@@ -21,37 +19,24 @@ const users = [
 ];
 
 const siteConfig = {
-  title: "RecifeJs", // Title for your website.
+  title: "RecifeJs",
   tagline: "A powerful MVC Framework for GraphQL",
-  url: "https://recifejs.github.io", // Your website URL
-  baseUrl: "/", // Base URL for your project */
-  // For github.io type URLs, you would set the url and baseUrl like:
-  //   url: 'https://facebook.github.io',
-  //   baseUrl: '/test-site/',
-
-  // Used for publishing and more
+  url: "https://recifejs.github.io",
+  baseUrl: "/",
   projectName: "recife",
   organizationName: "recifejs",
-  // For top-level user or org sites, the organization is still the same.
-  // e.g., for the https://JoelMarcey.github.io site, it would be set like...
-  //   organizationName: 'JoelMarcey'
-
-  // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
     { doc: "about", label: "Docs" },
     { blog: "blog", label: "Blog" },
     { href: "https://github.com/recifejs", label: "Github" }
   ],
 
-  // If you have users set above, you add it here:
   users,
 
-  /* path to images for header/footer */
   headerIcon: "img/favicon.ico",
   footerIcon: "",
   favicon: "img/favicon.ico",
 
-  /* Colors for website */
   colors: {
     primaryColor: "#062fa7",
     secondaryColor: "#042074"
@@ -71,38 +56,28 @@ const siteConfig = {
   },
   */
 
-  // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
   copyright: `Copyright © ${new Date().getFullYear()} RecifeJs`,
 
   highlight: {
-    // Highlight.js theme to use for syntax highlighting in code blocks.
-    theme: "default"
+    theme: "tomorrow-night-bright"
   },
 
-  // Add custom scripts here that would be placed in <script> tags.
-  scripts: ["https://buttons.github.io/buttons.js"],
+  scripts: [
+    "https://buttons.github.io/buttons.js",
+    "https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.0/clipboard.min.js",
+    "/js/code-block-button.js"
+  ],
+  stylesheets: ["/css/code-block-button.css"],
 
-  // On page navigation for the current documentation page.
   onPageNav: "separate",
-  // No .html extensions for paths.
   cleanUrl: true,
 
-  // Open Graph and Twitter card images.
   ogImage: "img/undraw_online.svg",
   twitterImage: "img/undraw_tweetstorm.svg",
+  docsSideNavCollapsible: false,
 
-  // For sites with a sizable amount of content, set collapsible to true.
-  // Expand/collapse the links and subcategories under categories.
-  // docsSideNavCollapsible: true,
-
-  // Show documentation's last contributor's name.
-  // enableUpdateBy: true,
-
-  // Show documentation's last update time.
-  // enableUpdateTime: true,
-
-  // You may provide arbitrary config keys to be used as needed by your
-  // template. For example, if you need your repo's URL...
+  enableUpdateBy: true,
+  enableUpdateTime: true,
   repoUrl: "https://github.com/recifejs/recife"
 };
 
